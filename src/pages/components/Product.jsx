3813e560a8a90d52215b34b41c283a6638ee6aa8
freshie-Dev <hazardpubg1122@gmail.com>
2023-08-21 23:05:35 +0500

    testing node.js workflow

--- src/pages/components/Product.jsx
+++ src/pages/components/Product.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
+// import SingleProduct from '../SingleProduct';
 
 export default function Product(props) {
     const {data} = props;
 
   return (
-    <div key={data.id} className='flex flex-col items-center justify-center p-4'>
+    <NavLink to={`/singleProduct/:${data.id}`} className='flex flex-col items-center justify-center p-4'>
         <img src={data.image} alt={data.title} className='w-[200px] h-[200px] object-contain'/>
         <h1 className='text-xl font-semibold'>{data.title}</h1>
         <p className='text-sm text-gray-500'>{data.description}</p>
-    </div>
+    </NavLink>
   )
 }
